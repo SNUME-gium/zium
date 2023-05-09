277c168a127fde08d5cc61678c69b1bcee3cc1a9
zium5532 <130118952+zium5532@users.noreply.github.com>
2023-05-09 23:06:29 +0900

    Add files via upload

--- skeleton.py
+++ skeleton.py
@@ -1,29 +1,27 @@
 # !/usr/bin/env python3
 import rospy
 import numpy as np
-import cv2
 
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Header
 
 class DetermineColor:
-	def __init__(self):
-		self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
-		self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
-		self.bridge = CvBridge()
-		self.count = 0
+    def __init__(self):
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
+        self.color_pub = rospy.Publisher('/rotate_cmd', Header, queue_size=10)
+        self.bridge = CvBridge()
 
-	def callback(self, data):
-		try:
+    def callback(self, data):
+        try:
             # listen image topic
-			image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
 
             # prepare rotate_cmd msg
             # DO NOT DELETE THE BELOW THREE LINES!
-			msg = Header()
-			msg = data.header
-			msg.frame_id = '0'  # default: STOP
+            msg = Header()
+            msg = data.header
+            msg.frame_id = '0'  # default: STOP
 
             # determine background color
             # TODO
@@ -31,33 +29,70 @@ class DetermineColor:
             # msg.frame_id = '+1' # CCW (Blue background)
             # msg.frame_id = '0'  # STOP
             # msg.frame_id = '-1' # CW (Red background)
-			self.count += 1
-			if self.count > 300 and self.count < 600:
-				msg.frame_id = '+1'
-			elif self.count > 600 and self.count < 900:
-				msg.frame_id = '-1'
-			elif self.count > 900 and self.count < 1200:
-				msg.frame_id = '0'
-			elif self.count > 1200 and self.count < 1500:
-				msg.frame_id = '+1'
-			elif self.count > 1500 and self.count < 1800:
-				msg.frame_id = '-1'
-			elif self.count > 1800 and self.count < 2100:
-				msg.frame_id = '0'
+            
+            # 
+            color_dict = dict()
+            color_dict.clear()
+            max = 0
+            x = image.shape[0]
+            y = image.shape[1]
+            res = (0,0,0)
+            start = 0.2
+            end = 0.8
+			
+            for i in range(round(x*start),round(x*end)):
+                for j in range(round(y*start),round(y*end)):
+                    num = 15
 
+                    b = image[i][j][0]//num*num
+                    g = image[i][j][1]//num*num
+                    r = image[i][j][2]//num*num
+                
+                    if (b,g,r) in color_dict:
+                        color_dict[(b,g,r)]+=1
+                        if(color_dict[(b,g,r)]>max):
+                            max = color_dict[(b,g,r)]
+                            res = (b,g,r)
+                    else:
+                        color_dict[(b,g,r)]=1
+            # blue +1  // red -1
+            
+            color_names={
+                (255,255,255):'White',
+                (0,0,0):'Black',
+                (0,0,255):'Red',
+                (0,255,0):'Green',
+                (255,0,0):'Blue',
+                (0,255,255):'Yellow',
+                (255,0,255):'Pink',
+                (255,128,0):'SkyBlue'
+            }
+            
+            min_dist = 100000000
+            min_color = 'Black'
+            
+            for key in color_names:
+                dist = (res[0]-key[0])**2+(res[1]-key[1])**2+(res[2]-key[2])**2
+                if dist < min_dist:
+                    min_dist = dist
+                    min_color = color_names[key]
+            if min_color=='Red':
+                msg.frame_id = '-1'
+            elif min_color=='Blue' or min_color=='SkyBlue':
+                msg.frame_id = '+1'
+            
+            #msg.frame_id = str(res)
             # publish color_state
-			self.color_pub.publish(msg)
+            #msg.frame_id = min_color
+            self.color_pub.publish(msg)
 
-			cv2.imshow('Image', image)
-			cv2.waitKey(1)
-
-		except CvBridgeError as e:
-			print(e)
+        except CvBridgeError as e:
+            print(e)
 
 
-	def rospy_shutdown(self, signal, frame):
-		rospy.signal_shutdown("shut down")
-		sys.exit(0)
+    def rospy_shutdown(self, signal, frame):
+        rospy.signal_shutdown("shut down")
+        sys.exit(0)
 
 if __name__ == '__main__':
     detector = DetermineColor()
